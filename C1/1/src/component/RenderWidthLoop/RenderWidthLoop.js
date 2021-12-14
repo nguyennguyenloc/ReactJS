9d7540fa0a5148d5c2096105be0390d7fb1adcd6
nguyennguyenloc <nnloc123@gmail.com>
2021-12-14 22:05:57 +0700

    upload_bai_tap_slide_14

--- C1/1/src/component/RenderWidthLoop/RenderWidthLoop.js
+++ C1/1/src/component/RenderWidthLoop/RenderWidthLoop.js
@@ -11,28 +11,46 @@ export default class RenderWithLoop extends Component {
     {
       id: 2,
       name: "red car",
-      price: 1000,
+      price: 2000,
       img: "./carbasic/products/red-car.jpg",
     },
     {
       id: 3,
       name: "silver car",
-      price: 1000,
+      price: 3000,
       img: "./carbasic/products/silver-car.jpg",
     },
     {
       id: 4,
       name: "steel car",
-      price: 1000,
+      price: 4000,
       img: "./carbasic/products/steel-car.jpg",
     },
   ];
 
+  //   renderTable = () => {
+  //     let mangTrComponent = [];
+  //     for (let index = 0; index < this.productList.length; index++) {
+  //       let product = this.productList[index];
+  //       let trJSX = (
+  //         <tr key={index}>
+  //           <td>{product.id}</td>
+  //           <td>{product.name}</td>
+  //           <td>{product.price}</td>
+  //           <td>
+  //             <img style={{ width: "100px" }} src={product.img} />
+  //           </td>
+  //         </tr>
+  //       );
+  //       console.log(trJSX);
+  //       mangTrComponent.push(trJSX);
+  //     }
+  //     return mangTrComponent;
+  //   };
+
   renderTable = () => {
-    let mangTrComponent = [];
-    for (let index = 0; index < this.productList.length; index++) {
-      let product = this.productList[index];
-      let trJSX = (
+    let contentTable = this.productList.map((product, index) => {
+      return (
         <tr key={index}>
           <td>{product.id}</td>
           <td>{product.name}</td>
@@ -42,12 +60,9 @@ export default class RenderWithLoop extends Component {
           </td>
         </tr>
       );
-      console.log(trJSX);
-      mangTrComponent.push(trJSX);
-    }
-    return mangTrComponent;
+    });
+    return contentTable;
   };
-
   render() {
     return (
       <div className="container">

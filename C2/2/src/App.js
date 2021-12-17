d43c422a0d30093a0f3a800a972ede2cb0e68cfd
nguyennguyenloc <nnloc123@gmail.com>
2021-12-18 00:38:20 +0700

    upload_slide_3_P1

--- C2/2/src/App.js
+++ C2/2/src/App.js
@@ -1,11 +1,66 @@
 import React, { Component } from "react";
-import DemoProps from "./Props/DemoProps";
-
+import ProductList from "./Props/ProductList";
+// import DemoProps from "./Props/DemoProps";
+import dataJson from "./Data/data.json";
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <DemoProps />
+      <div className="container-fluid">
+        {/* <DemoProps /> */}
+        <div className="row">
+          <div className="col-4">
+            <div
+              style={{ minHeight: "800px" }}
+              className="nav flex-column nav-pills justify-content-center"
+              id="v-pills-tab"
+              role="tablist"
+              aria-orientation="vertical"
+            >
+              <a
+                className="nav-link active"
+                id="v-pills-home-tab"
+                data-toggle="pill"
+                href="#home"
+                role="tab"
+                aria-controls="home"
+                aria-selected="true"
+              >
+                Home
+              </a>
+              <a
+                className="nav-link"
+                id="v-pills-profile-tab"
+                data-toggle="pill"
+                href="#profile"
+                role="tab"
+                aria-controls="profile"
+                aria-selected="false"
+              >
+                Shop
+              </a>
+            </div>
+          </div>
+          <div className="col-8 productList">
+            <div className="tab-content" id="v-pills-tabContent">
+              <div
+                className="tab-pane fade show active"
+                id="home"
+                role="tabpanel"
+                aria-labelledby="v-pills-home-tab"
+              >
+                <ProductList arrProduct={dataJson} />
+              </div>
+              <div
+                className="tab-pane fade"
+                id="profile"
+                role="tabpanel"
+                aria-labelledby="v-pills-profile-tab"
+              >
+                Shop
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ C2/2/src/Props/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ProductItem from "./ProductItem";
+
+export default class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <ProductItem />
+      </div>
+    );
+  }
+}

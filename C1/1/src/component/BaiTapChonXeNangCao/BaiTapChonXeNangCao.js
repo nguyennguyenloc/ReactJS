8771cd549b50fd629774b262be2988aeb9b2e319
nguyennguyenloc <nnloc123@gmail.com>
2021-12-17 11:16:49 +0700

    upload_slide_21_xoay_xe_js_cloudimage_360

--- C1/1/src/component/BaiTapChonXeNangCao/BaiTapChonXeNangCao.js
+++ C1/1/src/component/BaiTapChonXeNangCao/BaiTapChonXeNangCao.js
@@ -10,7 +10,7 @@ export default class BaiTapChonXeNangCao extends Component {
       title: "Crystal Black",
       type: "Pearl",
       img: "./images/icons/icon-black.jpg",
-      srcImg: "./images/images-black/images-black-1/civic-1.jpg",
+      srcImg: "images-black/images-black-1/",
       color: "Black",
       price: "19,550",
       engineType: "In-Line 4-Cylinder",
@@ -35,9 +35,9 @@ export default class BaiTapChonXeNangCao extends Component {
     },
   };
 
-  changeImg = (carNew) => {
+  changeCar = (newCar) => {
     this.setState({
-      car: carNew,
+      car: newCar,
     });
   };
 
@@ -46,7 +46,7 @@ export default class BaiTapChonXeNangCao extends Component {
       return (
         <div
           onClick={() => {
-            this.changeImg(item);
+            this.changeCar(item);
           }}
           className="row border border-color-default mt-1 m-2"
           style={{ cursor: "pointer" }}
@@ -95,19 +95,44 @@ export default class BaiTapChonXeNangCao extends Component {
       );
     });
   };
+
+  componentDidMount = () => {
+    //đây là phương thức có sẵn của component tự động thực thi sau khi render được gọi, lưu ý componentditmount chỉ chạy 1 lần đầu tiên sau khi render thực thi
+    let tagScript = document.createElement("script");
+    tagScript.src =
+      "https://cdn.scaleflex.it/plugins/js-cloudimage-360-view/2.7.1/js-cloudimage-360-view.min.js";
+    document.querySelector("#appendScript").appendChild(tagScript);
+  };
+
+  componentDidUpdate = () => {
+    //clear ảnh cũ
+    document.querySelector("#carCurrent").innerHTML = "";
+    //hàm này chạy sau khi state thay đổi ( tự kích hoạt sau render)
+    // Lưu ý: không được setState ở component này
+    let tagScript = document.createElement("script");
+    tagScript.src =
+      "https://cdn.scaleflex.it/filerobot/js-cloudimage-360-view/v2.0.0.lazysizes.min.js";
+
+    //clear script cũ trước khi appenrd
+    document.querySelector("#appendScript").innerHTML = "";
+    document.querySelector("#appendScript").appendChild(tagScript);
+  };
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-6">
-            <div className="model">
-              <img
+            <div className="model" style={{ width: "100%" }}>
+              <div
+                id="carCurrent"
                 style={{ width: "100%" }}
-                src={this.state.car.srcImg}
-                //   src="../assets/products/black-car.jpg"
-                alt={this.state.car.color}
-              />
-              <div class="card mt-2">
+                className="cloudimage-360"
+                data-folder={"./images/" + this.state.car.srcImg}
+                data-filename="civic-{index}.jpg"
+                data-amount="8"
+              ></div>
+              <div id="appendScript"></div>
+              <div className="card mt-2">
                 <h5 div className="card-header text-default">
                   Exterior color
                 </h5>

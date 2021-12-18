5fa2c654e552d6bdcc411168ddb95f4008e5e7d5
nguyennguyenloc <nnloc123@gmail.com>
2021-12-18 11:12:38 +0700

    upload_slide_4_P2

--- C2/2/src/Props/ProductItem.js
+++ C2/2/src/Props/ProductItem.js
@@ -2,6 +2,31 @@ import React, { Component } from "react";
 
 export default class ProductItem extends Component {
   render() {
-    return <div></div>;
+    let { item } = this.props;
+    return (
+      <div className="card text-left">
+        <img
+          style={{ width: "300px" }}
+          className="card-img-top"
+          src={item.image}
+          alt={item.image}
+        />
+        <div className="card-body">
+          <h4 className="card-title">{item.name}</h4>
+          <p className="card-text">
+            {/* {item.description.length > 20 ? (
+          <p>{item.description.substring(0, 40)}</p>
+        ) : (
+          <p>{item.description}</p>
+        )} */}
+            {item.price}
+          </p>
+          <button className="w3-button w3-black">
+            {" "}
+            Add to cart <i class="fa fa-cart-arrow-down"></i>
+          </button>
+        </div>
+      </div>
+    );
   }
 }
--- C2/2/src/Props/ProductList.js
+++ C2/2/src/Props/ProductList.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import ProductItem from "./ProductItem";
 
 export default class ProductList extends Component {
+  renderProductItem = () => {
+    return this.props.arrProduct.map((item, index) => {
+      return (
+        <div
+          className="col-4 mt-3 w3-container w3-center w3-animate-zoom"
+          key={index}
+        >
+          <ProductItem item={item} />
+        </div>
+      );
+    });
+  };
   render() {
-    return (
-      <div>
-        <ProductItem />
-      </div>
-    );
+    return <div className="row">{this.renderProductItem()}</div>;
   }
 }
